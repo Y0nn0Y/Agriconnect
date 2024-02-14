4ba93fcaff71cea6b8da01e8bd0a90aba2cd9f34
Yon <beauyon31@gmail.com>
2024-02-14 08:35:22 +0100

    V1Test

--- Capteur.java
+++ Capteur.java
@@ -1,1 +1,74 @@
+import java.util.Random;
 
+public class Capteur {
+
+// Déclaration variable
+    private String code;
+    private double latitude;
+    private double longitude;
+    private float temperature;
+    private float humidite;
+
+// Constructeur (code et coordonnées)
+    public Capteur(String code, double latitude, double longitude) {
+        this.code = code;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+// Getters et setters
+    public String getcode() {
+        return code;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidite() {
+        return humidite;
+    }
+
+// Fonctions
+
+    //Génère une température aléatoirement
+    private double genereTemperature() {
+        Random random = new Random();
+        return -5 + random.nextDouble() * 30; // Température entre -5 et 30 degrés Celsius
+    }
+
+    //Génère une humidité aléatoirement
+    private double genereHumidite() {
+        Random random = new Random();
+        return random.nextDouble() * 100; // Humidité entre 0% et 100%
+    }
+
+    //Remonte une temperature a la central
+    public void remonterTemperature(CentraleGestion centrale) {
+        this.temperature = genereTemperature();
+        centrale.enregistrerTemperature(this.code, this.temperature);
+    }
+
+    //Remonte une humidite 
+    public void remonterHumidite(CentraleGestion centrale) {
+        this.humidite = genereHumidite();
+        centrale.enregistrerHumidite(this.code, this.humidite);
+    }
+
+    //Daclare un ajout auprès de la centrale
+    public void declarerAjout(CentraleGestion centrale) {
+        centrale.ajoutCapteur(this.code);
+    }
+
+    //Declare un retrait auprès de la centrale
+    public void declarerRetrait(CentraleGestion centrale) {
+        centrale.retirerCapteur(this.code);
+    }
